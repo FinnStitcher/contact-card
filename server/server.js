16ead468bdd7e53ca6974c0f65f6e601222cc831
Finn Stitcher <foundfootage@protonmail.com>
2022-09-07 11:13:58 -0700

    server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('../client/'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require('./routes/html-routes.js')(app);
+
+app.listen(PORT, () => {
+    console.log('http://localhost:3001');
+});
