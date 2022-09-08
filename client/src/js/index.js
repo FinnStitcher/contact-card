500ca2387ea0626ffcf2b1c4cbe9ee45dbce94e9
Finn Stitcher <foundfootage@protonmail.com>
2022-09-08 16:58:41 -0700

    update dom

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,25 +1,58 @@
-import './form';
-import './submit';
-import {initDb, getDb, postDb} from './database';
+import {toggleForm, clearForm} from './form';
+import {fetchCards} from './cards';
+import { initDb, getDb, postDb } from './database';
 
 import '../css/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import {Tooltip, Toast, Popover} from 'bootstrap';
+import { Tooltip, Toast, Popover } from 'bootstrap';
 
 import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.jpg';
 
-window.addEventListener('load', function() {
-    document.getElementById('logo').src = Logo;
-    document.getElementById('bearThumbnail').src = Bear;
-    document.getElementById('dogThumbnail').src = Dog;
+window.addEventListener('load', function () {
+	document.getElementById('logo').src = Logo;
+	document.getElementById('bearThumbnail').src = Bear;
+	document.getElementById('dogThumbnail').src = Dog;
 
-    // needs to go after the image declarations or the images dont load
-    initDb();
+	// needs to go after the image declarations or the images dont load
+	initDb();
 
-    getDb();
-    postDb('Lernantino', 'lernantino@test.com', 8186601234, 'Bear');
-    getDb();
+	fetchCards();
+});
+
+// Form functionality
+const form = document.getElementById('formToggle');
+const newContactButton = document.getElementById('new-contact');
+let submitBtnToUpdate = false;
+let profileId;
+
+newContactButton.addEventListener('click', event => {
+	toggleForm();
+});
+
+form.addEventListener('submit', event => {
+	// Handle data
+	event.preventDefault();
+	let name = document.getElementById('name').value;
+	let phone = document.getElementById('phone').value;
+	let email = document.getElementById('email').value;
+	let profile = document.querySelector('input[type="radio"]:checked').value;
+
+	// Post form data to IndexedDB OR Edit an existing card in IndexedDB
+	if (submitBtnToUpdate == false) {
+		postDb(name, email, phone, profile);
+	} else {
+		fetchCards();
+		// Toggles the submit button back to POST functionality
+		submitBtnToUpdate = false;
+	}
+
+	// Clear form
+	clearForm();
+	// Toggle form
+	toggleForm();
+	// Reload the DOM
+	fetchCards();
 });
